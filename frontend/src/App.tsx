39657e6a409d4ea89ee80bfe1c20b7e9bbdc96d1
natehc <chetanchetu4940@gmail.com>
2026-08-17 14:25:35 +0530

    feature: added Sidebar to App.tsx to enable switiching between Overview and Queue.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { Sidebar } from "./components/layout/Sidebar";
+
 export default function App() {
+  //this remembers which tab is currently active
+  const [activeTab, setActiveTab] = useState<"overview" | "queue">("queue");
+
   return (
-    <div className="min-h-screen bg-background text-white p-8">
-      <h1 className="text-3xl font-bold text-brand-red">
-        Fraud & Anomaly Control Center (Tailwind is working!)
-      </h1>
+    <div className="flex min-h-screen bg-background text-white">
+      {/* left sidebar */}
+      <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      {/* main content */}
+      <div className="flex-1 p-8 overflow-auto">
+        {/* simple paceholder for now */}
+        {activeTab === "overview" && (
+          <div>
+            <h1 className="text-3xl font-bol mb-4"> Overview Dashboard</h1>
+            <p className="text-gray-400">KPIs and Charts will go here...</p>
+          </div>
+        )}
+
+        {activeTab === "queue" && (
+          <div>
+            <h1 className="text-3xl font-bold mb-4">Reviewer Queue</h1>
+            <p className="text-gray-400">Table transactions will go here...</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
